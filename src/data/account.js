4e961274714287056dafccd82e2b751103089815
Jasper Sam <jaspersam824@gmail.com>
2026-09-14 02:08:38 +0800

    Added data for the account and cart

--- src/data/account.js
+++ src/data/account.js
@@ -1,3 +1,15 @@
-const account = [
-    {}
-]
+class accounts{
+    constructor(key){
+        
+    }
+    loadStorage(){
+
+    }
+    addAccount(){
+
+    }
+}
+
+class farmerAccounts extends accounts{
+
+}
